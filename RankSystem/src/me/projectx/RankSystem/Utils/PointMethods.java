a9ba91ad633f779cdfbfb93f07aba7975ce88b44
mazentheamazin <email@mazentheamazin.com>
2014-01-30 17:12:03 -0800

    Update PointMethods.java

--- RankSystem/src/me/projectx/RankSystem/Utils/PointMethods.java
+++ RankSystem/src/me/projectx/RankSystem/Utils/PointMethods.java
@@ -42,8 +42,13 @@ public class PointMethods {
 	 * @return The player's points
 	 */
 	public double getPlayerPoints(Player player){
-		double points = VaultMethods.economy.getBalance(p.getName())
-		VaultMethods.updateMoney(player);
+		FileConfiguration c = m.getPoints();
+		int points;
+		if (!(c.getString(player.getName()) == null)){
+			points = c.getInt(player.getName());
+		}else{
+			System.out.println("No points!");
+		}
 		return points;
 	}
 }
